6f7d7e0c9b5ddf49c378e3d4efd38a12d1288c2b
jgsch <jgschmidtbr@gmail.com>
2025-08-25 22:04:13 -0300

    feat: routes - /add_client e /remove_client

--- services/executor-comandos/src/routers/routes.ts
+++ services/executor-comandos/src/routers/routes.ts
@@ -5,8 +5,7 @@ import { addDomainBlockList, create_group, insert_client_in_group, remove_client
 function routes() {
 
     const router = express.Router()
-
-    //preciso colocar ESModule 
+ 
     router.post('/bloquear', async (req: Request, res: Response) => {
         //Colocar try catch
         const { dominio } = req.body;
@@ -19,7 +18,7 @@ function routes() {
 
             //lambda -> API -> front 
             if (response) {
-                res.status(200).json({ "message": `Domínio ${nome} adicionado a lista de bloqueios do grupo ${group}`, "staturs": "ok"});
+                res.status(200).json({ "message": `Domínio ${nome} adicionado a lista de bloqueios do grupo ${group}`, "status": "ok"});
             } else {
                 res.status(500).json({ "Error": 'Erro ao bloquear o domínio', "status": "error" });
             }
@@ -29,11 +28,39 @@ function routes() {
 
     router.post('/add_client', async (req: Request, res: Response) => {
 
+        const { client } = req.body;    
+
+        if(client) {
+            const client_address = client['client_address'] as string;
+            const group_name = client['group_name'] as string;
+
+            const response = await insert_client_in_group(client_address, group_name);
+
+            if (response) {
+                res.status(200).json({ "message": `Cliente ${client_address} foi inserido no grupo ${group_name} com sucesso`, "status": "ok"});
+            } else {
+                res.status(500).json({ "Error": `Não foi possivel inserir o cliente ${client_address} no grupo ${group_name}`, "status": "error" });
+            }
+        }
         
     })
 
     router.post('/remove_client', async (req: Request, res: Response) => {
         
+        const { client } = req.body;
+
+        if(client){
+            const client_address = client['client_address'] as string;
+            const group_name = client['group_name'] as string;
+
+            const response = await remove_client(client_address, group_name);
+
+            if (response) {
+                res.status(200).json({ "message": `Cliente  ${client_address} foi removido`, "status": "ok"});
+            } else {
+                res.status(500).json({ "Error": `Não foi possivel remover o cliente ${client_address}`, "status": "error" });
+            }
+        }
 
         
     })
